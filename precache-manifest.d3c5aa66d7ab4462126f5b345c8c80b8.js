2736eb18fc284404d9adbdd13acdc43bcf214f6a
superkencodes <kkimbrell85@gmail.com>
2020-10-01 23:15:39 -0500

    Updates

---
+++ precache-manifest.d3c5aa66d7ab4462126f5b345c8c80b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcaddeb6f84ddf38d647db2099da945",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "378dd31ab61d88e677d7",
+    "url": "/monsters-rolodex/static/css/main.c5caad80.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378dd31ab61d88e677d7",
+    "url": "/monsters-rolodex/static/js/main.ac3647f5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
